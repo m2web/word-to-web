44f367efb0b6b61ee2ec071b0dc60390725702a9
m2web <m2web@yahoo.com>
2022-05-14 14:20:52 -0400

    feat: :fire: Update convert word to md script

--- scripts/convertWordDocToMarkdown.sh
+++ scripts/convertWordDocToMarkdown.sh
@@ -1,18 +1,18 @@
 # loop through each markdown file in the dir
-for file in *; do
-    if [ "${file: -5}" == ".docx" ]
-    then
-        # use pandoc to convert the doc to markdown
-        echo $file
-        pandoc $file -t markdown --output=$file.md --extract-media=./images/$file
-    fi
 
-    #if word doc has a three letter extension, then it is a docx file and needs to be renamed
-    #so pandoc will convert it to markdown
+for file in *; do
+#if word doc has a three letter extension, then it is a docx file and needs to be renamed
+#so pandoc will convert it to markdown
     if [ "${file: -4}" == ".doc" ]
     then
         #rename word doc to docx
-        mv $file $file.docx
+        mv -v "$file" "${file%.doc}.docx"
+    fi
+done
+
+for file in *; do
+    if [ "${file: -5}" == ".docx" ]
+    then
         # use pandoc to convert the doc to markdown
         echo $file
         pandoc $file -t markdown --output=$file.md --extract-media=./images/$file
