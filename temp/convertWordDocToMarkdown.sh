086c01728a88827f6030d212bd53d3535f12cda5
m2web <m2web@yahoo.com>
2022-05-14 12:11:34 -0400

    feat: :fire: Add covertWordDocToMarkdown shell script

---
+++ temp/convertWordDocToMarkdown.sh
@@ -0,0 +1,21 @@
+# loop through each markdown file in the dir
+for file in *; do
+    if [ "${file: -5}" == ".docx" ]
+    then
+        # use pandoc to convert the doc to markdown
+        echo $file
+        pandoc $file -t markdown --output=$file.md --extract-media=./images/$file
+    fi
+
+    #if word doc has a three letter extension, then it is a docx file and needs to be renamed
+    #so pandoc will convert it to markdown
+    if [ "${file: -4}" == ".doc" ]
+    then
+        #rename word doc to docx
+        mv $file $file.docx
+        # use pandoc to convert the doc to markdown
+        echo $file
+        pandoc $file -t markdown --output=$file.md --extract-media=./images/$file
+    fi
+done;
+
