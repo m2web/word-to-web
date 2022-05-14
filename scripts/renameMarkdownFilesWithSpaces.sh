0f4ab3eaa2989ba70dc655287c3bc74f66a9ec99
m2web <m2web@yahoo.com>
2022-05-14 14:54:28 -0400

    feat: :zap: Update rename filename script to use right var

--- scripts/renameMarkdownFilesWithSpaces.sh
+++ scripts/renameMarkdownFilesWithSpaces.sh
@@ -1,7 +1,7 @@
-for file in *.md
-do
-    # echo $file
-    if [[ $string = *" "* ]]; then #the filename contains a space
+for file in *; do
+    if [[ $file = *" "* ]]; then #the filename contains a space
+        # echo $file
+        # update the filename with an underscore instead of a space
         mv "$file" "${file// /_}"
     fi
 done
