da985d541b318cae97cc8bae82883482a3d72d12
m2web <m2web@yahoo.com>
2022-05-14 11:10:01 -0400

    doc: :pencil2: Update with comments

--- scripts/cleanMarkdown.sh
+++ scripts/cleanMarkdown.sh
@@ -1,9 +1,12 @@
-# TODO: loop through each markdown file in the dir
+# loop through each markdown file in the dir
 for file in *; do
     if [ "${file: -3}" == ".md" ] 
     then
         echo $file;
+        # change image folder path to the parent dir
         sed -i 's/.image/..image/' $file
+        
+        # remove the image width and height settings created by pandoc
         sed -i 's/width=.+//' $file
         sed -i 's/height=.+//' $file
     fi
