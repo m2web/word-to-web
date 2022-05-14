77c354937b68d6752202f0847d94d362209441ed
m2web <m2web@yahoo.com>
2022-05-14 14:39:05 -0400

    feat: :zap: Update prepend frontmatter script
    Commenting echo statement and removed old commented code

--- scripts/prepend-frontmatter.sh
+++ scripts/prepend-frontmatter.sh
@@ -2,23 +2,20 @@
 
 for file in *.md
 do
-    echo $file
+    # echo $file
     
     theDate=$(date '+%Y-%m-%dT%H:%M:%S-4:00')
-
-    #remove any frontmatter created by pandoc conversion from *docx to markdown
-    # sed '1 { /^---/ { :a N; /\n---/! ba; d} }' $file > $file.temp.md
 	
 	#replace '_' with space in file name for post title
 	theTitle=${file//_/ }
 	
-# Prepend front-matter to files
-TEMPLATE="---
-title: \"$theTitle\"
-date: $theDate
-draft: true
----
-"
+    # Prepend front-matter to files
+    TEMPLATE="---
+    title: \"$theTitle\"
+    date: $theDate
+    draft: true
+    ---
+    "
 
     echo "$TEMPLATE" | cat - "$file" > temp && mv temp "$file"
 done
