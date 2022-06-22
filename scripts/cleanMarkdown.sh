a0566b33d27b162d385f188847823336d5d792f4
Mark McFadden <m2web@yahoo.com>
2022-06-22 19:23:59 -0400

    feat: :fire: Update cleanMarkdown.sh
    Update images path from relative to off the root.

--- scripts/cleanMarkdown.sh
+++ scripts/cleanMarkdown.sh
@@ -3,8 +3,8 @@ for file in *; do
     if [ "${file: -3}" == ".md" ] 
     then
         # echo $file;
-        # change image folder path to the relative path
-        sed -i 's/(.\/images/(images/' $file
+        # change image folder path
+        sed -i 's/(.\/images/(\/images/' $file
         
         # remove the image width and height settings created by pandoc
         sed -i 's/{width=.*//' $file
